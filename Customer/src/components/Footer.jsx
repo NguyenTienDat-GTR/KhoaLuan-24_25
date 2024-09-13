99c317bbee46827b731301ec26566884fa12ae34
datgtr001 <19473791.dat@student.iuh.edu.vn>
2024-09-13 22:34:23 +0700

    update responsive for footer

--- Customer/src/components/Footer.jsx
+++ Customer/src/components/Footer.jsx
@@ -1,115 +1,253 @@
 import React from "react";
-import { Container, Grid, Typography, Box, Link } from "@mui/material";
+import { Container, Grid, Typography, Link, Box } from "@mui/material";
+import PinDropOutlinedIcon from "@mui/icons-material/PinDropOutlined";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import PhoneInTalkOutlinedIcon from "@mui/icons-material/PhoneInTalkOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 
 const Footer = () => {
   return (
-    <footer>
+    <Container
+      maxWidth="lg"
+      sx={{
+        width: { xs: "100vw", sm: "100vw", md: "100vw" },
+        margin: "2rem 0",
+        padding: { xs: "0", sm: "0", md: "0" },
+      }}
+    >
       <Box
-        maxWidth="lg"
         sx={{
-          width: { xs: "100%", sm: "100%", md: "100vw" },
-          // overflow: "hidden",
+          backgroundColor: "#59cbdf",
+          width: "100vw",
+          height: "auto",
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" }, // Column on mobile, row on tablet and desktop
+          justifyContent: "space-between",
+          alignItems: { xs: "flex-start", sm: "center" }, // Align items for mobile and larger screens
+          gap: { sm: "2", md: "0" },
+          marginX: { xs: "0", sm: "0", md: "0" },
         }}
       >
+        {/* Contact Section */}
         <Box
+          className="container-contact"
           sx={{
-            width: { xs: "100%", sm: "100%", md: "100vw" },
             display: "flex",
-            flexDirection: { xs: "column", sm: "row", md: "row" }, // Chiều dọc trên mobile, hàng ngang trên desktop và tablet
-            justifyContent: {
-              xs: "space-evenly",
-              sm: "center",
-              md: "center",
-            }, // Căn đều các cột trên desktop và tablet
-            alignItems: "flex-start",
-            m: "1rem auto",
-            backgroundColor: "rgba(21,182,210,0.72)",
-            padding: { xs: "1rem", sm: "2rem", md: "2rem" },
-            overflow: "hidden",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: { xs: "100%", sm: "30%", md: "35%" }, // Adjust widths for mobile/tablet/desktop
+            height: { xs: "auto", sm: "auto", md: "auto" },
+            mt: { xs: "2rem", sm: "0", md: "0" },
           }}
         >
           <Box
-            className="information"
             sx={{
-              flex: 1,
-              marginBottom: {
-                xs: "1rem",
-                md: "0",
-                // width: { xs: "100%", sm: "33%", md: "33%" },
-                flex: 1,
-              },
+              display: "flex",
+              flexDirection: "column",
+              width: "80%",
+              height: "auto",
+              ml: { xs: "0", sm: "2rem", md: "2rem" },
             }}
           >
-            <Typography variant="h6" gutterBottom>
+            <Typography
+              sx={{
+                color: "#fff",
+                fontSize: {
+                  xs: "1rem",
+                  sm: "1rem",
+                  md: "1.5rem",
+                },
+                fontWeight: "bold",
+              }}
+            >
+              Thông tin liên hệ
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{
+                color: "red",
+                fontWeight: 700,
+                fontSize: { xs: "1.5rem", sm: "1.5rem", md: "2rem" },
+              }}
+            >
               NHA KHOA HOÀNG KIM
             </Typography>
-            <Box>
-              <Typography>
-                Địa chỉ: <br />
-                12, đường Nguyễn Văn Bảo, phường 4, Quận Gò Vấp
-              </Typography>
-              <Typography>Giờ làm việc: ...</Typography>
-              <Typography>Hotline: ...</Typography>
-              <Typography>Email: ...</Typography>
-              <Typography>Website: ...</Typography>
-            </Box>
-          </Box>
 
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: {
+                  xs: "0.8rem",
+                  sm: "1rem",
+                  md: "1.2rem",
+                },
+              }}
+            >
+              <p style={{ textAlign: "justify", textIndent: "30px" }}>
+                <p></p>
+                <PinDropOutlinedIcon /> <b>Địa chỉ:</b>
+                <p></p>
+                <AccessTimeOutlinedIcon /> <b>Giờ làm việc:</b>
+                <p></p>
+                <PhoneInTalkOutlinedIcon />
+                <b> Hotline:</b>
+                <p></p>
+                <EmailOutlinedIcon /> <b>Email:</b>
+                <p></p>
+                <LanguageOutlinedIcon /> <b>Website:</b>
+              </p>
+            </Typography>
+          </Box>
+        </Box>
+
+        {/* Policy Section */}
+        <Box
+          className="container-policy"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: { xs: "100%", sm: "30%", md: "30%" },
+            height: { xs: "auto", sm: "15rem", md: "25rem" },
+            mt: { xs: "2rem", sm: "0", md: "0" },
+          }}
+        >
           <Box
-            className="information"
             sx={{
-              flex: 1,
-              marginBottom: {
-                xs: "1rem",
-                md: "0",
-                // width: { xs: "100%", sm: "33%", md: "50px" },
-                flex: 1,
-              },
+              display: "flex",
+              flexDirection: "column",
+              width: "80%",
+              height: "auto",
+              ml: { xs: "0", sm: "2rem", md: "2rem" },
             }}
           >
-            <Typography variant="h6" gutterBottom>
+            <Typography
+              sx={{
+                color: "#fff",
+                fontSize: {
+                  xs: "1rem",
+                  sm: "1rem",
+                  md: "1.5rem",
+                },
+                fontWeight: "bold",
+              }}
+            >
               CHÍNH SÁCH - HỖ TRỢ
             </Typography>
-            <Link href="/chinh-sach-bao-mat" sx={{ display: "block" }}>
-              Chính sách bảo mật
-            </Link>
-            <Link href="/chinh-sach-bao-hanh" sx={{ display: "block" }}>
-              Chính sách bảo hành
-            </Link>
-            {/* Thêm các link khác */}
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: {
+                  xs: "0.8rem",
+                  sm: "1rem",
+                  md: "1.2rem",
+                },
+              }}
+            >
+              <ul>
+                <p style={{ textAlign: "justify" }}>
+                  <li>Chính sách bảo mật thông tin khách hàng</li>
+                  <li>Chính sách bảo hành các dịch vụ</li>
+                  <li>Chính sách thanh toán</li>
+                  <li>Giấy phép hoạt động</li>
+                  <li>Hướng dẫn đặt lịch hẹn</li>
+                </p>
+              </ul>
+            </Typography>
           </Box>
+        </Box>
 
+        {/* Social Section */}
+        <Box
+          className="container-social"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: { xs: "100%", sm: "30%", md: "30%" },
+            height: { xs: "auto", sm: "15rem", md: "20rem" },
+            mt: { xs: "2rem", sm: "0", md: "0" },
+          }}
+        >
           <Box
-            className="information"
             sx={{
-              flex: 1,
-              //  width: { xs: "100%", sm: "33%",
-              md: "auto",
+              display: "flex",
+              flexDirection: "column",
+              width: "80%",
+              height: "auto",
+              ml: { xs: "0", sm: "2rem", md: "2rem" },
             }}
           >
-            <Typography variant="h6" gutterBottom>
+            <Typography
+              sx={{
+                color: "#FFF",
+                fontSize: {
+                  xs: "1rem",
+                  sm: "1rem",
+                  md: "1.5rem",
+                },
+                fontWeight: "bold",
+              }}
+            >
               LIÊN KẾT MẠNG XÃ HỘI
             </Typography>
-            {/* Các icon mạng xã hội */}
-            <Typography variant="h6" gutterBottom>
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: {
+                  xs: "0.8rem",
+                  sm: "1rem",
+                  md: "1.2rem",
+                },
+              }}
+            >
+              <FacebookOutlinedIcon />
+            </Typography>
+            <Typography
+              sx={{
+                color: "#FFF",
+                fontSize: {
+                  xs: "1rem",
+                  sm: "1rem",
+                  md: "1.5rem",
+                },
+                fontWeight: "bold",
+              }}
+            >
               ĐỐI TÁC THANH TOÁN
             </Typography>
-            {/* Các logo đối tác */}
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: {
+                  xs: "0.8rem",
+                  sm: "1rem",
+                  md: "1.2rem",
+                },
+              }}
+            >
+              <FacebookOutlinedIcon />
+            </Typography>
           </Box>
         </Box>
-
-        <Box mt={3}>
-          <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright © "}
-            {new Date().getFullYear()}
-            {" Nha Khoa HOÀNG KIM - "}
-            <Link color="inherit" href="/">
-              12, đường Nguyễn Văn Bảo, phường 4, Quận Gò Vấp
-            </Link>
-          </Typography>
-        </Box>
       </Box>
-    </footer>
+
+      <Box mt={3}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Copyright © "}
+          {new Date().getFullYear()}
+          {" Nha Khoa HOÀNG KIM - "}
+          <Link color="inherit" href="/">
+            12, đường Nguyễn Văn Bảo, phường 4, Quận Gò Vấp
+          </Link>
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
